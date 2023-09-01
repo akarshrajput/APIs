9857a69603dac639d24ef1b181de8f7092e7b87b
Akarsh Rajput <112371948+akarshrajput@users.noreply.github.com>
2023-09-01 10:16:03 +0530

    Update Country API Website

--- Countries API/script.js
+++ Countries API/script.js
@@ -7,7 +7,7 @@ const grid = document.querySelector(".grid");
 const renderCountry = function (data, className = "") {
   const html = `
   <div class="country">
-          <img class="c-flag" src="flag.png" />
+          <img class="c-flag" src="${data[0].flags.png}" />
           <div class="c-info">
             <p class="c-name">${data[0].name.common}</p>
             <p class="c-continent">${data[0].continents}</p>
@@ -36,7 +36,7 @@ const renderCountry = function (data, className = "") {
                 d="M239.15,212.42l-56-112a8,8,0,0,0-14.31,0l-21.71,43.43A88,88,0,0,1,100,126.93,103.65,103.65,0,0,0,127.69,64H152a8,8,0,0,0,0-16H96V32a8,8,0,0,0-16,0V48H24a8,8,0,0,0,0,16h87.63A87.76,87.76,0,0,1,88,116.35a87.74,87.74,0,0,1-19-31,8,8,0,1,0-15.08,5.34A103.63,103.63,0,0,0,76,127a87.55,87.55,0,0,1-52,17,8,8,0,0,0,0,16,103.46,103.46,0,0,0,64-22.08,104.18,104.18,0,0,0,51.44,21.31l-26.6,53.19a8,8,0,0,0,14.31,7.16L140.94,192h70.11l13.79,27.58A8,8,0,0,0,232,224a8,8,0,0,0,7.15-11.58ZM148.94,176,176,121.89,203.05,176Z"
               ></path>
             </svg>
-            <p class="c-language">${data[0].languages}</p>
+            <p class="c-capital">${data[0].capital}</p>
           </div>
           <div class="flex">
             <svg
@@ -49,7 +49,7 @@ const renderCountry = function (data, className = "") {
                 d="M184,89.57V84c0-25.08-37.83-44-88-44S8,58.92,8,84v40c0,20.89,26.25,37.49,64,42.46V172c0,25.08,37.83,44,88,44s88-18.92,88-44V132C248,111.3,222.58,94.68,184,89.57ZM232,132c0,13.22-30.79,28-72,28-3.73,0-7.43-.13-11.08-.37C170.49,151.77,184,139,184,124V105.74C213.87,110.19,232,122.27,232,132ZM72,150.25V126.46A183.74,183.74,0,0,0,96,128a183.74,183.74,0,0,0,24-1.54v23.79A163,163,0,0,1,96,152,163,163,0,0,1,72,150.25Zm96-40.32V124c0,8.39-12.41,17.4-32,22.87V123.5C148.91,120.37,159.84,115.71,168,109.93ZM96,56c41.21,0,72,14.78,72,28s-30.79,28-72,28S24,97.22,24,84,54.79,56,96,56ZM24,124V109.93c8.16,5.78,19.09,10.44,32,13.57v23.37C36.41,141.4,24,132.39,24,124Zm64,48v-4.17c2.63.1,5.29.17,8,.17,3.88,0,7.67-.13,11.39-.35A121.92,121.92,0,0,0,120,171.41v23.46C100.41,189.4,88,180.39,88,172Zm48,26.25V174.4a179.48,179.48,0,0,0,24,1.6,183.74,183.74,0,0,0,24-1.54v23.79a165.45,165.45,0,0,1-48,0Zm64-3.38V171.5c12.91-3.13,23.84-7.79,32-13.57V172C232,180.39,219.59,189.4,200,194.87Z"
               ></path>
             </svg>
-            <p class="c-currency">${data.currencies}</p>
+            <p class="c-area">${data[0].area}</p>
           </div>
         </div>
   `;
@@ -65,5 +65,12 @@ const countryData = function (country) {
       renderCountry(data);
     });
 };
-countryData("india");
-countryData("portugal");
+const targetCountry = document.querySelector(".target-country");
+const searchButton = document.querySelector(".search-button");
+
+searchButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  const c = String(targetCountry.value);
+  countryData(c);
+  e.preventDefault();
+});
